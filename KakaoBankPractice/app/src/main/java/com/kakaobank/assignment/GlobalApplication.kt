de16abea8e4556dd2b303da8c05d9fb7ac81f6d0
HamBeomJoon <hbj0802@naver.com>
2024-11-14 18:43:22 +0900

    feat(app): GlobalApplication 설정

---
+++ KakaoBankPractice/app/src/main/java/com/kakaobank/assignment/GlobalApplication.kt
@@ -0,0 +1,12 @@
+package com.kakaobank.assignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
