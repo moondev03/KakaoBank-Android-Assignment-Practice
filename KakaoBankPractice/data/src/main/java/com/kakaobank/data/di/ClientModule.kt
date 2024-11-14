652f2792f71430c8ad017de2a67a5f083f3e63d7
HamBeomJoon <hbj0802@naver.com>
2024-11-14 19:06:43 +0900

    feat(data:ClientModule): clientModule 추가
    - HttpLoggingInterceptor 추가
    - OkHttpClient 추가
    - Retrofit provider 추가

---
+++ KakaoBankPractice/data/src/main/java/com/kakaobank/data/di/ClientModule.kt
@@ -0,0 +1,47 @@
+package com.kakaobank.data.di
+
+import com.google.gson.GsonBuilder
+import com.kakaobank.data.BuildConfig
+import com.kakaobank.data.util.PrettyJsonLogger
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ClientModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor(PrettyJsonLogger()).apply {
+        if (BuildConfig.DEBUG) setLevel(HttpLoggingInterceptor.Level.BODY)
+        else setLevel(HttpLoggingInterceptor.Level.NONE)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+            .client(okHttpClient)
+            .baseUrl(BuildConfig.BASE_URL)
+            .build()
+    }
+}
